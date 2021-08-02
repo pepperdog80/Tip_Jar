b80bbb4f98521762c76930a688fba83e7c453c0a
Sydney McGinnis <77169150+pepperdog80@users.noreply.github.com>
2021-08-02 14:26:35 -0400

    Initial commit

---
+++ Prework2/Prework/Main_MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Main_MenuViewController.swift
+//  Prework
+//
+//  Created by Sydney m on 8/2/21.
+//
+
+import UIKit
+
+class Main_MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
